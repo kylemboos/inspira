2497ffacd0532b4b3342f8df03dd6d5bfbe478d0
kylemboos <kyle.boos21@gmail.com>
2013-07-17 21:23:24 -0700

    place holder for tutors and testimonials

--- testimonials.php
+++ testimonials.php
@@ -77,36 +77,9 @@
 
       <hr>
 
-      <div class="jumbotron">
-        <h1>We Aim to Inspire!</h1>
-        <p class="lead">Need some sort of intro to the company or a smaller about us... could be a blurb to lead to signing up, checking out pricing, contact us, or meet the tutors.</p>
-        <a class="btn btn-large btn-success" href="#">Meet the Tutors</a>
-      </div>
-
-      <hr>
-
-      <div class="row-fluid marketing">
-        <div class="span6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
-
-        <div class="span6">
-          <h4>Subheading</h4>
-          <p>Donec id elit non mi porta gravida at eget metus. Maecenas faucibus mollis interdum.</p>
-
-          <h4>Subheading</h4>
-          <p>Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Cras mattis consectetur purus sit amet fermentum.</p>
-
-          <h4>Subheading</h4>
-          <p>Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
-        </div>
+     <div class="jumbotron">
+        <h1>Coming Soon!</h1>
+       
       </div>
 
       <hr>
